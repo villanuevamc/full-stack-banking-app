73766b4667a01a7381e8112dea29e4d75778c1a5
Mariah Villanueva <mariah.c.villanueva@gmail.com>
2022-12-08 04:31:18 -0500

    Debugging

--- public/deposit.js
+++ public/deposit.js
@@ -21,6 +21,7 @@ function Deposit() {
         },
       })
         .then((res) => {
+          console.log("res: ", res);
           if (!res.ok) {
             throw new Error(`${res}`);
           }
@@ -66,7 +67,6 @@ function Deposit() {
       },
     })
       .then((res) => {
-        console.log("res: ", res);
         if (!res.ok) {
           throw new Error(`${res}`);
         }
